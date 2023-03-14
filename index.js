e5b15ead6f7e89fa2ede90e57d22ec21778a2370
Federico Pena <federpena22@gmail.com>
2023-03-14 00:11:08 -0300

    codigo js chat gpt

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* llamar elementos del dom */
-const botones = document.querySelector(".botones")
+/* const botones = document.querySelector(".botones")
 const numeros = document.getElementsByClassName("numero")
 const operadores = document.getElementsByClassName("operador")
 const igual = document.querySelector(".igual")
@@ -27,3 +27,88 @@ console.log(borrar) */
 
 /* evento de escucha */
 
+
+
+//CHAT GPT
+// Selección de elementos HTML
+const pantallaCuenta = document.querySelector('.cuenta');
+const pantallaResultado = document.querySelector('.resultado');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+const botonIgual = document.querySelector('.igual');
+const botonBorrar = document.querySelector('.borrar');
+
+let cuenta = '';
+let operador = '';
+let resultado = null;
+
+// Función para actualizar la pantalla
+function actualizarPantalla() {
+  pantallaCuenta.innerText = cuenta;
+  pantallaResultado.innerText = resultado;
+}
+
+// Función para realizar una operación
+function calcular() {
+  let valor1 = parseFloat(resultado);
+  let valor2 = parseFloat(cuenta);
+
+  switch (operador) {
+    case '+':
+      resultado = valor1 + valor2;
+      break;
+    case '-':
+      resultado = valor1 - valor2;
+      break;
+    case '*':
+      resultado = valor1 * valor2;
+      break;
+    case '/':
+      resultado = valor1 / valor2;
+      break;
+    default:
+      resultado = cuenta;
+  }
+
+  cuenta = '';
+  operador = '';
+}
+
+// Asignación de eventos a los botones de números
+botonesNumeros.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    cuenta += boton.innerText;
+    actualizarPantalla();
+  });
+});
+
+// Asignación de eventos a los botones de operadores
+botonesOperadores.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    if (resultado === null) {
+      resultado = cuenta;
+    } else if (operador) {
+      calcular();
+    }
+
+    operador = boton.innerText;
+    cuenta = '';
+    actualizarPantalla();
+  });
+});
+
+// Asignación de eventos al botón de igual
+botonIgual.addEventListener('click', () => {
+  if (operador) {
+    calcular();
+    actualizarPantalla();
+  }
+});
+
+// Asignación de eventos al botón de borrar
+botonBorrar.addEventListener('click', () => {
+  cuenta = '';
+  operador = '';
+  resultado = null;
+  actualizarPantalla();
+});
